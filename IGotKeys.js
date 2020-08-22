0d6457f32b620596f311bb850173aea3a36bd09f
Orben <42387077+torben123456@users.noreply.github.com>
2020-08-22 17:37:20 +0200

    Update IGotKeys.js

--- IGotKeys.js
+++ IGotKeys.js
@@ -1,3 +1,20 @@
-alert('hello world!');
+const script = document.createElement("script")
+ script.type = "text/javascript"
+script.src = "https://code.jquery.com/jquery-3.5.1.slim.js"
+			  integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM="
+			  crossorigin="anonymous"
+  integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM="
+  crossorigin="anonymous"
+document.head.appendChild(script)
 
-alert('to');
+eval(script)
+
+console.log('jquery loaded')
+
+
+
+
+ var code = e.keyCode || e.which;
+ if(code == 13) { 
+ 	alert('enter');
+ }
